7dcd43f5c42272c0623d9b8f66b48275d84f0d32
nimishjain1000 <nimishjain1000@gmail.com>
2016-04-01 17:44:59 +0530

    initial commit

---
+++ app/src/main/java/com/example/nimish/courtcounter/MainActivity.java
@@ -0,0 +1,80 @@
+package com.example.nimish.courtcounter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+  int scoreTeamA=0;
+  int scoreTeamB=0;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void reset(View view)
+    { scoreTeamA=0;
+      scoreTeamB=0;
+      displayForTeamA(scoreTeamA);
+      displayForTeamB(scoreTeamB);
+
+
+    }
+    public void free(View view)
+    {   switch(view.getId())
+    {
+      case R.id.team_a_free:   scoreTeamA+=1;
+        displayForTeamA(scoreTeamA);
+// handle button A click;
+        break;
+      case R.id.team_b_free:   scoreTeamB+=1;
+        displayForTeamB(scoreTeamB);
+// handle button B click;
+        break;
+      default:
+        throw new RuntimeException("Unknow button ID");
+
+    }
+    }
+    public void inc2(View view)
+    {
+      switch(view.getId())
+      {
+        case R.id.team_a_plus2:   scoreTeamA+=2;
+          displayForTeamA(scoreTeamA);
+// handle button A click;
+          break;
+        case R.id.team_b_plus2:   scoreTeamB+=2;
+          displayForTeamB(scoreTeamB);
+// handle button B click;
+          break;
+        default:
+          throw new RuntimeException("Unknow button ID");
+
+    }}
+    public void inc3(View view)
+    {  switch(view.getId())
+    {
+      case R.id.team_a_plus3:   scoreTeamA+=3;
+        displayForTeamA(scoreTeamA);
+// handle button A click;
+        break;
+      case R.id.team_b_plus3:   scoreTeamB+=3;
+        displayForTeamB(scoreTeamB);
+// handle button B click;
+        break;
+      default:
+        throw new RuntimeException("Unknow button ID");
+
+    }
+    }
+    public void displayForTeamA(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.team_a_score);
+        scoreView.setText(String.valueOf(score));
+    }
+  public void displayForTeamB(int score) {
+    TextView scoreView = (TextView) findViewById(R.id.team_b_score);
+    scoreView.setText(String.valueOf(score));
+  }
+}
